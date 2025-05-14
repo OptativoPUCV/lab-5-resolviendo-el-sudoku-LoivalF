88fb0896085183f16ab14f60717451d0308a81bb
Your Name <you@example.com>
2025-05-14 09:19:40 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -43,15 +43,59 @@ void print_node(Node* n){
     printf("\n");
 }
 
-int is_valid(Node* n){
+int is_valid(Node* n) {
 
-    return 1;
+  //para filas
+  for (int i = 0 ; i < 9 ; i++) {
+    int fila[10] = {0};
+    for (int j = 0 ; j < 9 ; j++) {
+      int valor = n->sudo[i][j];
+      if (fila[valor]) return 0;
+      fila[valor] = 1;
+    }
+  }
+
+  //para columnas
+  for (int j = 0 ; j < 9 ; j++) {
+    int columna[10] = {0};
+    for (int i = 0 ; i < 9 ; i++) {
+      int valor = n->sudo[i][j];
+      if (columna[valor]) return 0;
+      columna[valor] = 1;
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  int k=4,p; 
+  for(p=0;p<9;p++){
+    int i=3*(k/3) + (p/3) ;
+    int j=3*(k%3) + (p%3) ;
+    printf("%d ",nodo->sudo[i][j]);
+    if(p%3 == 2) printf("\n");
+}
+  return 1;
 }
 
 
-List* get_adj_nodes(Node* n){
-    List* list=createList();
+List* get_adj_nodes(Node* n) {
+  List* list=createList();
+  if (is_final(n) == 0)
     return list;
+
+
+    
+  return list;
 }
 
 
@@ -67,7 +111,8 @@ int is_final(Node* n) {
   return 1;
 }
 
-Node* DFS(Node* initial, int* cont){
+Node* DFS(Node* initial, int* cont) {
+
   return NULL;
 }
 
